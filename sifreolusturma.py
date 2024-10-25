be14f56aa610d8abb12fbc1fcf2f534a759cc8c8
Burak6167-glicth <burakbulbul1461@gmail.com>
2024-10-25 22:11:11 +0300

    Create sifreolusturma.py

---
+++ sifreolusturma.py
@@ -0,0 +1,11 @@
+import random
+
+karakterler = "æß€^%&\_|¨é+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+sifre_uzunlugu = int(input("Şifre Uzunluğunuzu Girin: "))
+
+sifre = ""
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+
+print(sifre)
